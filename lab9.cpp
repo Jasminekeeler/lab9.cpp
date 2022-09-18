5a13c6a74b9226b3a96852a2b5c44f06ba566471
Jasminekeeler <113307521+Jasminekeeler@users.noreply.github.com>
2022-09-18 00:03:25 -0500

    Create lab9.cpp

---
+++ lab9.cpp
@@ -0,0 +1,33 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+  const int MIN_NUMBER = 0,
+            MAX_NUMBER = 4;
+  int num;
+  int t;
+  int h ;
+  double distance;
+  
+  cout << fixed << setprecision(2) << endl;
+  
+  cout << "\nPlease input the time of fall in seconds:" << endl;
+	cin >> t;    
+   cout << "\nPlease input the height of the bridge in meters:" << endl;  
+  cin >> h;
+  cout << "Time Falling (seconds) Distance Fallen (meters)";
+  cout << "***********************************************\n";
+ for (num = MIN_NUMBER; num <= MAX_NUMBER; num++)
+      cout << num << "\t\t" << 0.5 * 9.8*(num * num) << endl;
+      distance =  0.5 * 9.8*(num * num);
+  
+ if (distance > h)
+		{
+			cout << " Warning Bad Data: The distance Fallen exceeds the height of the bridge. ";
+		}
+return 0;
+}
